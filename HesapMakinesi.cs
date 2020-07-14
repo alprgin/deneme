8f783ffa72e33707bd006893c3ac1ac96ecdca2e
alprgin <68145736+alprgin@users.noreply.github.com>
2020-07-14 15:12:38 +0300

    project pertama

---
+++ HesapMakinesi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class HesapMakinesi
+    {
+
+       
+            static void Main(string[] args)
+            {
+                For(100);
+                Console.ReadLine();
+            }
+
+            private static void For(int hedef, int n = 0)
+            {
+                if (n == hedef)
+                {
+                    return;
+                }
+                Console.WriteLine(n);
+                For(hedef, ++n);
+            }
+    }
+}
